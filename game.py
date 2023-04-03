360cf92b854c64bd55e48fd814d1ff06deb3fb91
Sharon <smayguamendiola@gmail.com>
2023-04-03 10:37:02 -0400

    funcionando

--- game.py
+++ game.py
@@ -406,23 +406,6 @@ def terminal_test(table):  # jugador actual #recibiria tambien el turno no
 
     return False
 
-def actions(table, piece):
-  res = []
-  for i in range(len(table)):
-    for j in range(len(table[i])):
-      if table[i][j]==piece:
-        # Mueves
-        f, c = i,j
-        c_table = copy.deepcopy(table)
-        for k in range(8):
-          nf, nc = f + cf[k], c + cc[k]
-          if nf>=0 and nf<4 and nc>=0 and nc<4:
-            if c_table[nf][nc]=='_':
-              c_table[f][c]='_'
-              c_table[nf][nc] = piece
-              res.append(c_table)
-  return res # devuelve una lista de matrices que son las acciones
-
 
 def choose_piece():
     piece = input("¿Qué ficha deseas jugar? (X / O): ").upper()
@@ -441,104 +424,88 @@ if human_piece == 'X':
 else:
     ai_piece = 'X'
 
+import copy
 
-def juego():
-    print("Juego cautela")
-    turno = 1
-    # Una ficha = [fila,columna,nombre]
-    fichas_negras = [[0, 0, "X"], [1, 1, "X"], [2, 2, "X"], [3, 3, "X"]]
-    fichas_blancas = [[3, 0, "O"], [2, 1, "O"], [1, 2, "O"], [0, 3, "O"]]
-    tabla = tabla_por_defecto(crear_tabla(), fichas_negras)
-    tabla = tabla_por_defecto(tabla, fichas_blancas)
+human_piece = choose_piece()
+if human_piece == 'X':
+    ai_piece = 'O'
+else:
+    ai_piece = 'X'
 
-    while True:
-        os.system("clear")
+fichas_negras = [[0, 0, "X"], [1, 1, "X"], [2, 2, "X"], [3, 3, "X"]]
+fichas_blancas = [[3, 0, "O"], [2, 1, "O"], [1, 2, "O"], [0, 3, "O"]]
+tabla = tabla_por_defecto(crear_tabla(), fichas_negras)
+tabla = tabla_por_defecto(tabla, fichas_blancas)
+mostrar_tabla(tabla)
+
+cf = [-1, -1, 0, 1, 1, 1, 0, -1]
+cc = [0, 1, 1, 1, 0, -1, -1, -1]
+
+
+def actions(tabla, piece):
+    res = []
+    for i in range(len(tabla)):
+        for j in range(len(tabla[i])):
+            if tabla[i][j] == piece:
+                for k in range(8):
+                    nf, nc = i + cf[k], j + cc[k]
+                    if 0 <= nf < len(tabla) and 0 <= nc < len(tabla[i]) and tabla[nf][nc] == '-':
+                        new_tabla = copy.deepcopy(tabla)
+                        new_tabla[i][j] = '-'
+                        new_tabla[nf][nc] = piece
+                        res.append(new_tabla)
+    return res
+
+
+juego = True
+while juego == True:
+
+    if ai_piece == 'O':
+        print("Fichas negras del jugador 1 Tú")
+        ##minmax_decision(tabla, "O")
+        mostrar_fichas_del_jugador(fichas_negras)
+        numero_ficha = int(input("Elije tu ficha: "))
+        movimiento = input("Elige tu Movimiento (N,S,E,O,NE,NO,SE,SO): ")
+        ficha = fichas_negras[numero_ficha - 1]
+        fichas_negras[numero_ficha - 1], tabla = mover_ficha(tabla, ficha, movimiento)
         mostrar_tabla(tabla)
-        print(tabla)  # list of lists, returns the final state
 
-        if ai_piece == 'O':
-            if turno % 2 == 0:
-                print("Fichas blancas del jugador 2: IA")
-                # minmax_decision(tabla, "X")
-                mostrar_fichas_del_jugador(fichas_blancas)
-                print('tabla')
-
-                action_ai, value = alpha_beta_pruning(tabla)
-                tabla = copy.deepcopy(action_ai)
-                print(tabla)
-
-                # print("Elección de ficha: ", FICHA_ELEGIDA_POR_IA)
-                # print("Elección de Movimiento (N,S,E,O,NE,NO,SE,SO): ", MOVIMIENTO_ELEGIDO_POR_IA)
-                # ficha = fichas_blancas[FICHA_ELEGIDA_POR_IA - 1]
-                # fichas_blancas[numero_ficha - 1] = mover_ficha(tabla, ficha, MOVIMIENTO_ELEGIDO_POR_IA)
+        print("Fichas blancas del jugador 2: IA")
+        # minmax_decision(tabla, "X")
+        mostrar_fichas_del_jugador(fichas_blancas)
+        action_ai, value = alpha_beta_pruning(tabla)
+        tabla = []
+        tabla = copy.deepcopy(action_ai)
+        mostrar_tabla(tabla)
 
 
-            else:
-                # turno humano con X juega primero
-                print("Fichas negras del jugador 1 Tú")
-                ##minmax_decision(tabla, "O")
-                mostrar_fichas_del_jugador(fichas_negras)
-                numero_ficha = int(input("Elije tu ficha: "))
-                movimiento = input("Elige tu Movimiento (N,S,E,O,NE,NO,SE,SO): ")
-                ficha = fichas_negras[numero_ficha - 1]
-                fichas_negras[numero_ficha - 1] = mover_ficha(tabla, ficha, movimiento)
 
-        else:
-            if turno % 2 == 0:
-                print("Fichas blancas del jugador 2: Tú")
-                # minmax_decision(tabla, "X")
-                mostrar_fichas_del_jugador(fichas_blancas)
-                numero_ficha = int(input("Elije tu ficha: "))
-                movimiento = input("Elige tu Movimiento (N,S,E,O,NE,NO,SE,SO): ")
-                ficha = fichas_blancas[numero_ficha - 1]
-                fichas_blancas[numero_ficha - 1] = mover_ficha(tabla, ficha, movimiento)
+    else:
+        # turno ia con X
+        # turno humano con X juega primero
+        print("Fichas negras del jugador 1: IA")
+        ##minmax_decision(tabla, "O")
+        mostrar_fichas_del_jugador(fichas_negras)
+        action_ai, value = alpha_beta_pruning(tabla)
+        tabla = []
+        tabla = copy.deepcopy(action_ai)
+        mostrar_tabla(tabla)
 
-            else:
-                # turno ia con X
-                # turno humano con X juega primero
-                print("Fichas negras del jugador 1: IA")
-                ##minmax_decision(tabla, "O")
-                mostrar_fichas_del_jugador(fichas_negras)
+        print("Fichas blancas del jugador 2: Tú")
+        # minmax_decision(tabla, "X")
+        mostrar_fichas_del_jugador(fichas_blancas)
+        numero_ficha = int(input("Elije tu ficha: "))
+        movimiento = input("Elige tu Movimiento (N,S,E,O,NE,NO,SE,SO): ")
+        ficha = fichas_blancas[numero_ficha - 1]
+        fichas_blancas[numero_ficha - 1], tabla = mover_ficha(tabla, ficha, movimiento)
+        mostrar_tabla(tabla)
 
-                print('tabla')
-                print(tabla)
-                action_ai, value = alpha_beta_pruning(tabla)
-                tabla = copy.deepcopy(action_ai)
-                print(tabla)
-
-                # print("Elección de ficha: ", FICHA_ELEGIDA_POR_IA)
-                # print("Elección de Movimiento (N,S,E,O,NE,NO,SE,SO): ", MOVIMIENTO_ELEGIDO_POR_IA)
-                # ficha = fichas_negras[FICHA_ELEGIDA_POR_IA - 1]
-                # fichas_negras[numero_ficha - 1] = mover_ficha(tabla, ficha, MOVIMIENTO_ELEGIDO_POR_IA)
-            # ficha = input("Insertar ficha en (fila,columna,ficha) o salir: ")
-            # datos_ficha = ficha.split(sep=",")
-
-        if verificar_victoria(tabla):
-            if turno % 2 == 0:
-                print("¡Victoria para las fichas blancas!")
-            else:
-                print("¡Victoria para las fichas negras!")
-            break
-        if verificar_victoria2(tabla):
-            if turno % 2 == 0:
-                print("¡Victoria para las fichas blancas!")
-            else:
-                print("¡Victoria para las fichas negras!")
-            break
-        if verificar_victoria3(tabla):
-            if turno % 2 == 0:
-                print("¡Victoria para las fichas blancas!")
-            else:
-                print("¡Victoria para las fichas negras!")
-            break
+    if verificar_victoria(tabla) == True or verificar_victoria2(tabla) == True or verificar_victoria3(tabla) == True:
+        print('el juego ha terminado')
+        juego = False
+    else:
         continuar = input("Desea Continuar (S,N): ")
         if continuar == "N":
-            return False
+            juego = False
         else:
-            # tabla = insertar_en_tabla(datos_ficha,tabla)
-            # print(datos_ficha)
-            turno += 1
-
-
-if __name__ == "__main__":
-    juego()
+            juego = True
